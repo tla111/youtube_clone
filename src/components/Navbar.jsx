bf12cc78085b61ff534efbe2884a976e43fc5a51
Tim La <tlamarketing111@gmail.com>
2022-10-04 09:30:40 -0400

    creating layout for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Stack } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { logo } from '../utils/constants';
 
-const Navbar = () => {
-    return (
-        <div>Navbar</div>
-    )
-}
+const Navbar = () => (
+    <Stack
+        direction="row"
+        alignItems="center"
+        p={2}
+        sx={{ position: "sticky", background: "#000", top: 0, justifyContent: "space-between" }}
+    >
 
-export default Navbar
+    </Stack>
+);
+
+export default Navbar;
