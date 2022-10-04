3d74064557f6beccd63444461b5097bbed657a26
Tim La <tlamarketing111@gmail.com>
2022-10-04 09:17:46 -0400

    set up components to be rendered in app component

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ import { Box } from '@mui/material';
 const App = () => (
     <BrowserRouter>
         <Box sx={{ backgroundColor: "#000" }}>
-            Navbar
+            {/* <Navbar/> */}
             <Routes>
                 {/* <Route path="/" exact element={{<Feed/>}} /> */}
+                {/* <Route path="/video/:id" element={{<VideoDetail />}} />
+                <Route path="/channel/:id" element={{<ChannelDetail />}} />
+                <Route path="/search/:searchTerm" element={{<SearchFeed/>}} /> */}
             </Routes>
         </Box>
     </BrowserRouter>
