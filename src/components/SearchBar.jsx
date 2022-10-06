df3da058475732a5d95f70fd9777ae9e726ee52d
Tim La <tlamarketing111@gmail.com>
2022-10-06 11:14:02 -0400

    implemented a search button

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Paper, IconButton } from '@mui/material';
-import { SearchIcon } from '@mui/icons-material';
+import { Search } from '@mui/icons-material';
 
 const SearchBar = () => {
     return (
@@ -22,6 +22,9 @@ const SearchBar = () => {
                 value=""
                 onChange={() => { }}
             />
+            <IconButton type="submit" sx={{ p: "10", color: "red" }}>
+                <Search />
+            </IconButton>
         </Paper>
     )
 }
