721338c3ca613a327dcb0740161382a31db0702c
Tim La <tlamarketing111@gmail.com>
2022-10-06 11:12:53 -0400

    created & styled searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,7 +5,24 @@ import { SearchIcon } from '@mui/icons-material';
 
 const SearchBar = () => {
     return (
-        <div>SearchBar</div>
+        <Paper
+            component="form"
+            onSubmit={() => { }}
+            sx={{
+                borderRadius: 20,
+                border: "1px solid #e3e3e3",
+                pl: 2,
+                boxShadow: "none",
+                mr: { sm: 5 }
+            }}
+        >
+            <input
+                className="search-bar"
+                placeholder="Search..."
+                value=""
+                onChange={() => { }}
+            />
+        </Paper>
     )
 }
 
